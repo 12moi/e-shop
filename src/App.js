b26c6e1c8ae34c9a042077f274fa36245ca713b7
devmoi <moishadrack.com>
2022-11-15 16:23:22 +0300

    add changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react'
-// import { BrowserRouter as Router } from 'react-router-dom'
-// import Header from './components/Header'
-// import Main from './components/Main'
-// import Footer from './components/Footer'
-// import Cart from './components/Cart'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import Cart from './components/Cart'
 
 const App = () => {
   return (
     <>
-      {/* <Router> */}
-        <div>Hello</div>
-        {/* <Header /> */}
-        {/* <Main />
-        <Footer /> */}
-      {/* </Router> */}
-      {/* <Cart /> */}
+       <Router> 
+        
+         <Header /> 
+         <Main />
+        <Footer /> 
+        </Router> 
+        <Cart /> 
     </>
   )
 }
