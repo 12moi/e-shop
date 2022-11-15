9e0e96ae2c38b1440d079c2df83ed234349ad87d
devmoi <moishadrack.com>
2022-11-15 18:33:45 +0300

    add changes

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,18 +1,3 @@
-import React from 'react'
+import Cart from './Cart'
 
-
-const index = () => {
- 
-
-  return (
-  
-      // eslint-disable-next-line react/jsx-no-undef, react/jsx-no-undef
-      <div>
-       <h1>Hey</h1>
-        
-      </div>
-    
-  )
-}
-
-export default index 
+export default Cart
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,27 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import { Route, Switch } from 'react-router-dom'
+import Home from '../pages/Home'
+import Products from '../pages/Products'
+import Contact from '../pages/Contact'
+import routes from '../constants/routes.json'
 
 const Main = () => {
- 
-
   return (
-  
-      // eslint-disable-next-line react/jsx-no-undef, react/jsx-no-undef
-      <div>
-       <h1>Hey</h1>
-        
-      </div>
-    
+    <MainWrapper>
+      <Switch>
+        <Route exact path={routes.HOME} component={Home} />
+        <Route exact path={routes.PRODUCTS} component={Products} />
+        <Route exact path={routes.CONTACT}component={Contact} />
+      </Switch>
+    </MainWrapper>
   )
 }
 
-export default Main 
+const MainWrapper = styled.main`
+  max-width: ${({ theme }) => theme.widths.content};
+  margin: 0 auto;
+  padding: 4rem;
+`
+
+export default Main
