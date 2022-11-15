0ff7b7cada21b127f30527a7508e885b41ec5d6e
devmoi <moishadrack.com>
2022-11-15 22:02:24 +0300

    Add theme styles

--- src/theme/Theme.js
+++ src/theme/Theme.js
@@ -1,18 +1,31 @@
-import React from 'react'
+const Theme = {
+  colors: {
+    primary: '#46FFD3',
+    red: '#ff9999',
+    dark: '#000',
+    light: '#FFF',
+    grey: {
+      light: '#F9F9F9',
+      main: '#E5E5E5',
+      dark: '#7B7B7B',
+    },
+    hover: {
+      primary: '#35eec2',
+      red: '#ee8888',
+      grey: '#d4d4d4',
+      dark: '#333',
+    },
+    active: {
+      primary: '#24ddb1',
+      red: '#dd7777',
+      grey: '#c3c3c3',
+      dark: '#444',
+    },
+  },
 
-
-const Theme = () => {
- 
-
-  return (
-  
-      // eslint-disable-next-line react/jsx-no-undef, react/jsx-no-undef
-      <div>
-       <h1>Hey</h1>
-        
-      </div>
-    
-  )
+  widths: {
+    content: '140rem',
+  },
 }
 
 export default Theme
